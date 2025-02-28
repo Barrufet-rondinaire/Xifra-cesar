fbf70fb24714f7e02444ef9a0ccb63b38c0a8923
Xavier Sala <fsala2@cendrassos.net>
2025-02-28 10:48:05 +0100

    chore: inici del programa

---
+++ Cesar/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cesar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var reader = new StreamReader("fitxer.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                Console.WriteLine(reader.ReadLine());
+            };
+        }
+        
+    }
+}

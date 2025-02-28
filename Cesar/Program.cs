154edb3b98edb3ea334219391b80c6b7a4862bb8
Xavier Sala <fsala2@cendrassos.net>
2025-02-28 16:46:07 +0100

    feat: solució

--- Cesar/Program.cs
+++ Cesar/Program.cs
@@ -4,13 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        using (var reader = new StreamReader("fitxer.txt"))
+        var abecedari = "ABCÇDEFGHIJKLMNOPQRSTUVWXYZ";
+        try
         {
+            using var writer = new StreamWriter("resultat.txt");
+            using var reader = new StreamReader("fitxe.txt");
             while (!reader.EndOfStream)
             {
-                Console.WriteLine(reader.ReadLine());
-            };
+                // var linia = reader.ReadLine();
+                //foreach (var lletra in linia)
+                // {
+
+                // }
+                var lletra = (char)reader.Read();
+                var lletraMajuscula = Char.ToUpper(lletra);
+                var posicio = abecedari.IndexOf(lletraMajuscula);
+                char novaLletra = ' ';
+                if (posicio == -1)
+                {
+                    novaLletra = lletra;
+                }
+                else
+                {
+                    posicio = (posicio + 4) % abecedari.Length;
+                    novaLletra = abecedari[posicio];
+                }
+
+                writer.Write(novaLletra);
+            }
+
+            ;
         }
-        
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("El archivo no existe");
+        }
+        catch(Exception e)
     }
 }
